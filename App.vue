eff810803fe8fc56bb85f57d10e02903c919ffc7
Cristella <cristellatiara@gmail.com>
2026-04-21 21:02:21 +0200

    Add files via upload
    This was used as the main layout for the entire app in order to ensure structure and readability was kept

---
+++ App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+
+  <router-view />
+</template>
+
+<style scoped></style>
